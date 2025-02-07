be7d5718ca735f417a2716f3df56cebdea6ede79
Adriano Saadeh <adrianosaadeh@gmail.com>
2025-02-07 15:54:35 -0300

    novo teste

--- projects/zombieplus/tests/leads.spec.js
+++ projects/zombieplus/tests/leads.spec.js
@@ -28,5 +28,23 @@ test('Deve cadastrar um lead na fila de espera', async ({ page }) => {
   await page.getByTestId('modal')
     .getByText('Quero entrar na fila!').click();
 
-  await page.waitForTimeout(5000);
+  // elemento flutuante -> toaster
+  await expect(page.locator('.toast')).toHaveText('Agradecemos por compartilhar seus dados conosco. Em breve, nossa equipe entrará em contato!');
+  await expect(page.locator('.toast')).toBeHidden({ timeout: 5000 })
+})
+
+test('Não deve cadastrar um lead na fila de espera', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  await page.getByRole('button', { name: /Aperte o play/ }).click();
+  await expect(page.getByTestId('modal').getByRole('heading')).toHaveText('Fila de espera')
+
+  await page.locator('#name').fill('Adriano Email Invalido');
+  await page.getByPlaceholder('Seu email principal').fill('ei.com');
+
+  await page.getByText('Quero entrar na fila!').click();
+
+  // elemento flutuante -> toaster
+  await expect(page.locator('.alert')).toHaveText('Email incorreto')
+
 })
