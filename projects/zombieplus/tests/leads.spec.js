18133e2ab6ab83be7a0270da2fbd973fb98e57ef
Adriano Saadeh <adrianosaadeh@gmail.com>
2025-02-09 10:14:22 -0300

    added negative tests

--- projects/zombieplus/tests/leads.spec.js
+++ projects/zombieplus/tests/leads.spec.js
@@ -33,7 +33,7 @@ test('Deve cadastrar um lead na fila de espera', async ({ page }) => {
   await expect(page.locator('.toast')).toBeHidden({ timeout: 5000 })
 })
 
-test('Não deve cadastrar um lead na fila de espera', async ({ page }) => {
+test('Não deve cadastrar um lead na fila de espera com e-mail incorreto', async ({ page }) => {
   await page.goto('http://localhost:3000');
 
   await page.getByRole('button', { name: /Aperte o play/ }).click();
@@ -46,5 +46,39 @@ test('Não deve cadastrar um lead na fila de espera', async ({ page }) => {
 
   // elemento flutuante -> toaster
   await expect(page.locator('.alert')).toHaveText('Email incorreto')
+})
 
+test('Não deve cadastrar quando o email não é preenchido', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  await page.getByRole('button', { name: /Aperte o play/ }).click();
+  await expect(page.getByTestId('modal').getByRole('heading')).toHaveText('Fila de espera')
+
+  await page.locator('#name').fill('Adriano Tester');
+
+  await page.getByText('Quero entrar na fila!').click();
+  await expect(page.locator('.alert')).toHaveText('Campo obrigatório')
+})
+
+test('Não deve cadastrar quando o nome não é preenchido', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  await page.getByRole('button', { name: /Aperte o play/ }).click();
+  await expect(page.getByTestId('modal').getByRole('heading')).toHaveText('Fila de espera')
+
+  await page.getByPlaceholder('Seu email principal').fill('adriano@gmail.com');
+
+  await page.getByText('Quero entrar na fila!').click();
+  await expect(page.locator('.alert')).toHaveText('Campo obrigatório')
+})
+
+test('Não deve cadastrar quando email e nome não sãp preenchidos', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  await page.getByRole('button', { name: /Aperte o play/ }).click();
+  await expect(page.getByTestId('modal').getByRole('heading')).toHaveText('Fila de espera')
+
+  await page.getByText('Quero entrar na fila!').click();
+  //passando um array para validar os 2 alerts
+  await expect(page.locator('.alert')).toHaveText(['Campo obrigatório', 'Campo obrigatório'])
 })
